907de30cc9cd22d8769afd667b343786234427a6
yosefHesham <yousefmobdev@gmail.com>
2022-06-10 17:14:35 +0200

    Style categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,7 +1,10 @@
   
   import React from "react"
   const Categories = () => {
-    return (<button style={{textAlign:"center"}}>Check Status</button>)
+    return (
+    <div style={{width:"100%", height:"100vh", display:"flex", justifyContent:"center", alignItems:"center"}}>
+    <button style={{textAlign:"center", margin:"0 auto"}}>Check Status</button>
+    </div>)
   }
 
   export default Categories;
