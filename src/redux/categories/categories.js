fab5455596c72136422af52ab66124f9fc0aa936
yosefHesham <yousefmobdev@gmail.com>
2022-06-14 17:23:05 +0200

    Create reducers and actionss

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+const CHECK_STATUS = "CHECK_STATUS";
+
+
+export default function categoriesReducer (state = [],action) {
+  switch(action.type) {
+    case CHECK_STATUS:
+      return "Under construction"
+      default:
+        return state
+  }
+
+
+}
+export const checkStatus = () => {
+  return {
+    type:CHECK_STATUS
+  }
+}
